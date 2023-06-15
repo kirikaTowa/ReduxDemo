e12ca960e654322dc73eed199b6c7970d1713c56
Yewj <2685872110@qq.com>
2023-06-15 15:15:13 +0800

    feat：第6节 Redux Dev Tools的安装

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,10 @@ import {legacy_createStore as createStore} from 'redux'
 import reducer from './reducer'
 
 /* 引入reduceer后  方法注入 */
-const store =createStore(reducer)
+const store =createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() /* 有这个插件就调用 */
+    )
 
 /* 暴露出去 */
 export default store
